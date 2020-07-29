72df4c24cd206c18e67686069c027f44861c2233
赵崇延 <47052088+ZhaoChongyan@users.noreply.github.com>
2020-07-29 09:08:25 +0800

    Add files via upload

--- jyKiller.cpp
+++ jyKiller.cpp
@@ -1,52 +1,53 @@
-#include <stdio.h>
+/*
+ * This program is created by ZhaoChongyan.
+ * This program is used for destroy Jifangguanlizhushou, A useless, disgusting malware software.
+ * The malware software is hard-opposing. So I created this program to solve the problem.
+ */
+#include <cstdlib>
 #include <windows.h>
 #include <TlHelp32.h>
-#include <stdlib.h>
-
-int Kill_Pro(TCHAR Kill_Name[]) {
-	HANDLE hSnapshort=CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
-	if(hSnapshort==INVALID_HANDLE_VALUE) {
-		printf("CreateToolhelp32Snapshot failed!\n");
-		return -1;
-	}
-	PROCESSENTRY32 stcProcessInfo;
-	stcProcessInfo.dwSize=sizeof(stcProcessInfo);
-	BOOL bRet=Process32First(hSnapshort,&stcProcessInfo);
-	while(bRet) {
-		if(!strcmp(stcProcessInfo.szExeFile,Kill_Name)) {
-			HANDLE hProcess=::OpenProcess(PROCESS_TERMINATE,FALSE,stcProcessInfo.th32ProcessID);
-			::TerminateProcess(hProcess,0);
-			CloseHandle(hProcess);
-		}
-		bRet=Process32Next(hSnapshort,&stcProcessInfo);
-	}
-	CloseHandle(hSnapshort);
-	return 0;
-}
-
-int Check_Pro(TCHAR Check_Name[]) {
-	HANDLE hSnapshort=CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS,0);
-	if(hSnapshort==INVALID_HANDLE_VALUE) {
-		printf("CreateToolhelp32Snapshot failed!\n");
-		return -1;
-	}
-	PROCESSENTRY32 stcProcessInfo;
-	stcProcessInfo.dwSize=sizeof(stcProcessInfo);
-	BOOL bRet=Process32First(hSnapshort,&stcProcessInfo);
-	while(bRet) {
-		if(!strcmp(stcProcessInfo.szExeFile,Check_Name))
-			return 1;
-		bRet=Process32Next(hSnapshort,&stcProcessInfo);
-	}
-	CloseHandle(hSnapshort);
-	return 0;
-}
+#include <queue>
+using namespace std;
 
 int main(void) {
-	puts("Killing jy");
-	while(Check_Pro("StudentMain.exe")) {
-		Kill_Pro("StudentMain.exe");
+	HANDLE hSnapshort = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hSnapshort == INVALID_HANDLE_VALUE) {
+		/*
+		 * Cannot creat a HANDLE!!!
+		 */
+		throw "CreateToolhelp32Snapshot failed!\n";
+		return -1;
 	}
-	puts("jy has been killed!");
+	queue<HANDLE> proList;
+	bool finished = false; 
+	do {
+		finished = true;
+		PROCESSENTRY32 stcProcessInfo;
+		stcProcessInfo.dwSize = sizeof(stcProcessInfo);
+		BOOL bRet = Process32First(hSnapshort, &stcProcessInfo);
+		while(bRet) {
+			/*
+			 * Add the process to the list.
+			 */
+			if(!strcmp(stcProcessInfo.szExeFile, "StudentMain.exe")) {
+				proList.push(::OpenProcess(PROCESS_TERMINATE, FALSE, stcProcessInfo.th32ProcessID));
+			}
+			bRet = Process32Next(hSnapshort, &stcProcessInfo);
+		}
+		while (proList.size()) {
+			/*
+			 * Abort the process in the list.
+			 */
+			if (!(::TerminateProcess(proList.front(), 0))) {
+				/*
+				 * Fail to abort the malware process.
+				 */
+				finished = false;
+			}
+			CloseHandle(proList.front());
+			proList.pop();
+		}
+	} while(!finished);
+	CloseHandle(hSnapshort);
 	return 0;
 }
